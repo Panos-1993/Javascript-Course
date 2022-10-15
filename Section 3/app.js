e52dbb44b74ef6fa277e5fc47c1c8f6307893563
Panos-1993 <panoslampros@icloud.com>
2022-10-15 15:30:31 +0300

    commit

--- Section 3/app.js
+++ Section 3/app.js
@@ -1,40 +1,136 @@
-let val;
+// Examining the DOM
 
-val = document;
-val = document.all;
-val = document.all[0];
-val = document.all.length;
-val = document.head;
-val = document.body;
-val = document.doctype;
-val = document.domain;
-val = document.URL;
-val = document.characterSet;
-val = document.contentType;
+// let val;
 
-val = document.forms;
-val = document.forms[0];
-val = document.forms[0].id;
-val = document.forms[0].method;
-val = document.forms[0].action;
+// val = document;
+// val = document.all;
+// val = document.all[0];
+// val = document.all.length;
+// val = document.head;
+// val = document.body;
+// val = document.doctype;
+// val = document.domain;
+// val = document.URL;
+// val = document.characterSet;
+// val = document.contentType;
 
-val = document.links;
-val = document.links[0];
-val = document.links[0].id;
-val = document.links[0].className;
-val = document.links[0].classList[0];
+// val = document.forms;
+// val = document.forms[0];
+// val = document.forms[0].id;
+// val = document.forms[0].method;
+// val = document.forms[0].action;
 
-val = document.images;
+// val = document.links;
+// val = document.links[0];
+// val = document.links[0].id;
+// val = document.links[0].className;
+// val = document.links[0].classList[0];
 
-val = document.scripts;
-val = document.scripts[2].getAttribute('src');
+// val = document.images;
 
-let scripts = document.scripts;
+// val = document.scripts;
+// val = document.scripts[2].getAttribute('src');
 
-let scriptsArr = Array.from(scripts);
+// let scripts = document.scripts;
 
-scriptsArr.forEach(function(script) {
-    console.log(script.getAttribute('src'));
-});
+// let scriptsArr = Array.from(scripts);
 
-console.log(val);
+// scriptsArr.forEach(function(script) {
+//     console.log(script.getAttribute('src'));
+// });
+
+// console.log(val);
+
+// // // DOM Selectors for single elements
+
+// // document.getElementById()
+
+// console.log(document.getElementById('task-title'));
+
+// // Get things from the element
+
+// console.log(document.getElementById('task-title').id);
+// console.log(document.getElementById('task-title').className);
+
+// const taskTitle = document.getElementById('task-title');
+
+// // Change styling
+
+// taskTitle.style.background = '#333';
+// taskTitle.style.color = '#FFF';
+// taskTitle.style.padding = '5px';
+// // taskTitle.style.display = 'none';
+
+// // Change content
+
+// taskTitle.textContent = 'Task List';
+// taskTitle.innerText = 'My Tasks';
+// taskTitle.innerHTML = '<span style="color:red">Task List</span>';
+
+// // document.querySelector()
+
+// console.log(document.querySelector('#task-title'));
+// console.log(document.querySelector('.card-title'));
+// console.log(document.querySelector('h5'));
+
+// document.querySelector('li').style.color = 'red';
+// document.querySelector('ul li').style.color = 'blue';
+
+// document.querySelector('li:last-child').style.color = 'red';
+// document.querySelector('li:nth-child(3)').style.color = 'yellow';
+// document.querySelector('li:nth-child(4)').textContent = 'Hello World';
+// document.querySelector('li:nth-child(odd)').style.background = '#ccc';
+// document.querySelector('li:nth-child(even)').style.background = '#f4f4f4';
+
+// // document.getElementsByClassName
+
+// const items = document.getElementsByClassName('collection-item');
+// console.log(items);
+// console.log(items[0]);
+// items[0].style.color = 'red';
+// items[3].textContent = 'Hello';
+
+// const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+
+// console.log(listItems);
+
+// // document.getElementsByTagName
+// let lis = document.getElementsByTagName('li');
+// console.log(lis);
+// console.log(lis[0]);
+// lis[0].style.color = 'red';
+// lis[3].textContent = 'Hello';
+
+// // Convert HTML Collection into Array
+// lis = Array.from(lis);
+
+// lis.forEach(function(li, index){
+//     console.log(li.className);
+//     li.textContent = `${index}: Hello`;
+// });
+
+// lis.reverse();
+
+// console.log(lis);
+
+// // document.querySelectorAll
+
+// const items = document.querySelectorAll('ul.collection li.colletion-item');
+
+// items.forEach(function(item, index){
+//     item.textContent = `${index}: Hello`;
+// });
+
+
+// const liOdd = document.querySelectorAll('li:nth-child(odd)');
+// const liEven = document.querySelectorAll('li:nth-child(even)');
+
+// liOdd.forEach(function(li, index){
+//     li.style.background = '#ccc';
+// });
+
+// for(let i = 0; i < liEven.length; i++){
+//     liEven[i].style.background = '#f4f4f4';
+// }
+
+// console.log(items);
