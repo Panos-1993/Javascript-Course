2dba11db10a71202b737c248673981f526c87f24
Panos-1993 <panoslampros@icloud.com>
2022-10-15 14:57:00 +0300

    commit

--- Section 3/app.js
+++ Section 3/app.js
@@ -2,7 +2,7 @@ let val;
 
 val = document;
 val = document.all;
-val = document.all[2];
+val = document.all[0];
 val = document.all.length;
 val = document.head;
 val = document.body;
@@ -34,7 +34,7 @@ let scripts = document.scripts;
 let scriptsArr = Array.from(scripts);
 
 scriptsArr.forEach(function(script) {
-  console.log(script.getAttribute('src'));
+    console.log(script.getAttribute('src'));
 });
 
 console.log(val);
