af3767b0c39920a302b00c0385717219c4faf23b
Panos-1993 <panoslampros@icloud.com>
2022-10-18 11:28:29 +0300

    project completed

--- Section 4/Number Guesser/app.js
+++ Section 4/Number Guesser/app.js
@@ -1,7 +1,7 @@
 1// Game values
 let min = 1,
     max = 10,
-    winningNum = 2,
+    winningNum = getRandomNum(min, max),
     guessesLeft = 3;
 
 // UI Elements
@@ -15,6 +15,13 @@ const game = document.querySelector('#game'),
 // Assign UI min and max
 minNum.textContent = min;
 maxNum.textContent = max;
+
+// Play again event listener
+game.addEventListener('mousedown', function(e){
+    if(e.target.className === 'play-again'){
+        window.location.reload();
+    }
+});
 
 // Add event listener for btn
 guessBtn.addEventListener('click', function(){
@@ -65,7 +72,17 @@ function gameOver(won, msg){
     message.style.color = color;
     // Set message
     setMessage(msg);   
+
+    // Play Again?
+    guessBtn.value = 'Play again';
+    guessBtn.className += 'play-again';
 }
+
+// Get winnning number
+function getRandomNum(min, max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
 
 // Set message
 function setMessage(msg, color){
