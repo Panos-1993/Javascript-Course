76beaed2ccd2a97730fa036cc66fcae42b37d6ea
Panos-1993 <panoslampros@icloud.com>
2022-11-09 12:39:08 +0200

    controllers & data structures app.js

---
+++ Section 12/app.js
@@ -0,0 +1,61 @@
+// Storage Controller
+
+
+
+// Item Controller
+const ItemCtrl = (function(){
+    // Item Constructor
+    const Item = function(id, name, calories){
+        this.id = id;
+        this.name = name;
+        this.calories = calories;
+    }
+
+    // Data Structure / State
+    const data = {
+        items: [
+            {id: 0, name: 'Steak Dinner', calories: 1200},
+            {id: 1, name: 'Eggs', calories: 200},
+            {id: 2, name: 'Cookie', calories: 100},
+        ],
+        currentItem: null,
+        totalCalories: 0
+    }
+
+    // Public Methods
+    return {
+        logData: function(){
+            return data;
+        }
+    }
+})();
+
+
+
+// UI Controller
+const UICtrl = (function(){
+    
+    // Public Methods
+    return {
+
+    }
+})();
+
+
+
+// App Controller
+const App = (function(ItemCtrl, UICtrl){
+
+    // Public methods
+    return {
+        init: function(){
+            console.log('Init App');
+        }
+    }
+    
+})(ItemCtrl, UICtrl);
+
+
+
+// Initialize App
+App.init();
